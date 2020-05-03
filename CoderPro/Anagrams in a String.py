b0421a1ef1655b8217c9cd7190c82e72135e96fd
Jie Hu <jieuhyL@gmail.com>
2020-05-02 22:24:22 -0700

    Update Anagrams in a String.py

--- CoderPro/Anagrams in a String.py
+++ CoderPro/Anagrams in a String.py
@@ -9,27 +9,27 @@ Created on Sat May  2 02:04:50 2020
 from collections import Counter
 class solution(object):
 
-    def findAnagrams(self, s, p):
-    
-        answer = []
-        m, n = len(s), len(p)
-        if m < n:
-            return answer
-        pCounter = Counter(p)
-        sCounter = Counter(s[:n-1])
-        print(pCounter)
-        print(sCounter)
-    
-        index = 0
-        for index in range(n - 1, m):
-                sCounter[s[index]] += 1
-                if sCounter == pCounter:
-                    answer.append(index - n + 1)
-                sCounter[s[index - n + 1]] -= 1
-                if sCounter[s[index - n + 1]] == 0:
-                    del sCounter[s[index - n + 1]]
-        return answer
+    def findana(s, p):
+        if len(s) < len(p):
+            return 0
+
+        ans = []    
+        dct_p = Counter(p)
+        dct_s = Counter(s[:len(p)])
+
+        if dct_p == dct_s:
+            ans.append(0)
+
+        for i in range(len(p), len(s)):
+            dct_s[s[i]] += 1
+            dct_s[s[i-len(p)]] -= 1
+            if dct_s[s[i-len(p)]] == 0:
+                del dct_s[s[i-len(p)]]
+            #print(dct_s)
+            if dct_s == dct_p:
+                ans.append(i-len(p)+1)
+        return ans
 
 s = "cbaebabacd"
 p = "abc"
-print(solution().findAnagrams(s, p))
+print(solution().findana(s, p))
